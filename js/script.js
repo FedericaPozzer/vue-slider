bb6fae1046df3287394165c6a155cd54af030d1b
Federica Pozzer <federica.pozzer@gmail.com>
2023-02-10 18:01:58 +0100

    static done

--- js/script.js
+++ js/script.js
@@ -13,42 +13,31 @@ createApp( {
                 },
 
                 {
-                    image: "img/02.webp",
+                    image: "./img/02.webp",
                     title: "Ratchet & Clank: Rift Apart",
                     text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
                 },
 
                 { 
-                    image: "img/03.webp",
+                    image: "./img/03.webp",
                     title: "Fortnite",
                     text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
                 },
 
                 {
-                    image: "img/04.webp",
+                    image: "./img/04.webp",
                     title: "Stray",
                     text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
                 },
 
                 {
-                    image: "img/05.webp",
+                    image: "./img/05.webp",
                     title: "Marvel's Avengers",
                     text: "Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
                 }
             ]
         }
     },
-
-    // methods: {
-    //     prev_image() {
-    //         if (this.movies.image - 1 < 0) {
-    //             this.movies.
-    //         }
-    //     },
-
-    // }
-
-
 }).mount("#my-app");
 
 
